a01b21bee17f0f80c18fb8ba81ecb0220d2c805d
ctrdae <81419410+ctrdae@users.noreply.github.com>
2022-07-12 01:21:23 +0900

    Update 125_daewon.py

--- String/[Leet] 125_Valid Palindrome/125_daewon.py
+++ String/[Leet] 125_Valid Palindrome/125_daewon.py
@@ -1,11 +1,13 @@
 class Solution(object):
     def isPalindrome(self, s):
-        pal = ""
+        pal = []
+        pal_str = ""
         for char in s:
-            if isinstance(char, str) == True:
+            if char.isalnum() == True:
                 pal.append(char.lower())
-        pal_reversed = "".join(reversed(pal))
-        if pal == pal_reversed:
+        pal_str = pal_str.join(pal)
+        pal_reversed = "".join(reversed(pal_str))
+        if pal_str == pal_reversed:
             return True
         else:
             return False
